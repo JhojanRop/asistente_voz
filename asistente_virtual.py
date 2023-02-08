48035a461a08e0bef90489c25a99931803394300
Jhojan_Ropero <jhojanropero0@gmail.com>
2023-02-07 22:46:29 -0500

    Saludo inicial

--- asistente_virtual.py
+++ asistente_virtual.py
@@ -3,6 +3,12 @@ import yfinance as yf
 import pyttsx3, pywhatkit, pyjokes, webbrowser, wikipedia
 import datetime
 
+#? Utilidades
+def ver_voces():
+  engine = pyttsx3.init()
+  for voz in engine.getProperty('voices'):
+    print(voz)
+
 #! Audio a texto
 def transformar_audio():
   r = sr.Recognizer()
@@ -33,7 +39,9 @@ voces = {
 
   'zira':'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0',
 
-  'david':'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0'
+  'david':'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0',
+
+  'helena':'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELENA_11.0',
 }
 
 #! Texto a voz
@@ -42,10 +50,6 @@ def hablar(mensaje):
   engine.setProperty('voice', voces['sabina'])
   engine.say(mensaje)
   engine.runAndWait()
-
-# engine = pyttsx3.init()
-# for voz in engine.getProperty('voices'):
-#   print(voz)
 
 #! Informar dia
 def dia():
@@ -63,4 +67,9 @@ def hora():
 
   hora = f'En este momento son las {hora.hour if hora.hour <= 12 else (hora.hour-12)} y {hora.minute} de {"la noche" if hora.hour >= 18 else ("la mañana" if hora.hour < 12 else ("el medio día" if hora.hour in range(12,13) else ()))}'
   hablar(hora)
-hora()
+# hora()
+
+#! Saludo inicial
+def saludo():
+  hablar(f'{"Buenos días" if datetime.datetime.now().hour < 12 else ("Buenas tardes" if datetime.datetime.now().hour > 11 and datetime.datetime.now().hour < 7 else "Buenas noches")} Yojan, ¿En qué te puedo ayudar?')
+saludo()
